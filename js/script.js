861cca329bb22052bffa5865b94caed476e7feaa
petrenko-am <petrenko.am@mail.ru>
2015-12-11 18:29:13 +0700

    module8-task1

--- js/script.js
+++ js/script.js
@@ -17,29 +17,55 @@
 		menu.classList.remove("modal-content-show");
 	});
 	
-	(function() {
-		if (!("FormData" in window)) {
-		return;
-		}
-		var form = document.querySelector(".form-field");
-		form.addEventListener("submit", function(event) {
-			event.preventDefault();
-			var data = new FormData(form);
-			request(data, function(response) {
-				console.log(response);
-			});
+	
+	if (!("FormData" in window)) {
+	return;
+	}
+	var form = document.querySelector(".form-field");
+	form.addEventListener("submit", function(event) {
+		event.preventDefault();
+		var data = new FormData(form);
+		request(data, function(response) {
+			console.log(response);
 		});
-		function request(data, fn) {
-			var xhr = new XMLHttpRequest();
-			var time = (new Date()).getTime();
-			xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time);
-			xhr.addEventListener("readystatechange", function() {
-				if (xhr.readyState == 4) {
-				fn(xhr.responseText);
-				}
-			});
-			xhr.send(data);
+	});
+	function request(data, fn) {
+		var xhr = new XMLHttpRequest();
+		var time = (new Date()).getTime();
+		xhr.open("post", "https://echo.htmlacademy.ru/adaptive?" + time);
+		xhr.addEventListener("readystatechange", function() {
+			if (xhr.readyState == 4) {
+			fn(xhr.responseText);
+			}
+		});
+		xhr.send(data);
+	}
+
+	var elements = document.querySelectorAll(".input-numeric");
+	for (var i = 0; i < elements.length; i++) {
+		initNumberField(elements[i]);
+	}
+	function initNumberField(parent) {
+		var input = parent.querySelector("input");
+		var minus = parent.querySelector(".input-numeric__minus");
+		var plus = parent.querySelector(".input-numeric__plus");
+		minus.addEventListener("click", function() {changeNumber(false);});
+		plus.addEventListener("click", function() {changeNumber(true);});
+		function changeNumber(operation) {
+			var value = Number(input.value);
+			if (isNaN(value)) {
+				value = 0;
+			}
+			if (operation) {
+				input.value = value + 1;
+			} 
+			else {
+				input.value = value - 1;
+			}
 		}
-	})();
+			
+	}
+
+
 
 })();
